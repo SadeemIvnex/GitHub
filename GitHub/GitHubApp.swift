02ee75b007e1f29098d4649e51d5f48a834b0734
Sadeem Orwoq <Sadeem6ivnex@gmail.com>
2024-09-26 16:42:57 +0300

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by Sadeem Orwoq on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
